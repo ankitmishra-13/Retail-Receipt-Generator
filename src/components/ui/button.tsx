8501b4ee6794ddf4951a675d6874b941a05fed94
Ankit Mishra <91165279+ankitmishra-13@users.noreply.github.com>
2025-05-13 01:43:35 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,6 +9,7 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant = "default", ...props }, ref) => {
   return (
     <button
+      suppressHydrationWarning
       className={cn(
         "inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
         variant === "default" && "bg-primary text-primary-foreground hover:bg-primary/90 px-4 py-2",
