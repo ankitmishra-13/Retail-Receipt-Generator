d0310f046523b5574949a22ade52e3c963279bf2
Ankit Mishra <91165279+ankitmishra-13@users.noreply.github.com>
2025-05-13 04:13:22 +0530

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,10 +4,9 @@ import { cn } from "@/lib/utils";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, value, ...props }, ref) => {
   return (
     <input
-      
       type={type ?? "text"}
       className={cn(
         "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -15,6 +14,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
       )}
       ref={ref}
       suppressHydrationWarning
+      value={
+        value === undefined || value === null || Number.isNaN(value)
+          ? ""
+          : value
+      }
       {...props}
     />
   );
